f7366eaa1620537acd31f63612ed38cb78ead640
lasscco-spec <lasscco@gmail.com>
2026-04-01 19:28:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-const CACHE = 'daytracker-v2';
-const ASSETS = ['./', './index.html', './manifest.json', './1.png', './2.png', './3.png', './icon-192.png', './icon-512.png'];
+const CACHE = "daytracker-clean-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./1.png", "./2.png", "./3.png"];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
-  ));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  const req = e.request;
-  e.respondWith(
-    caches.match(req).then(r => {
-      if (r) return r;
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(hit => {
+      if (hit) return hit;
       return fetch(req).catch(() => {
-        if (req.mode === 'navigate') return caches.match('./index.html');
+        if (req.mode === "navigate") return caches.match("./index.html");
         return Response.error();
       });
     })
